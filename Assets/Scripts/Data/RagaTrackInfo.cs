f1a931f13d9baccc5ef577a5b0b993106d4d591c
mappatel2 <mappatel2@gmail.com>
2026-01-10 11:06:34 -0500

    Updated GitIgnore file and Replaced Single Instruments system to tagging track using instruments

--- Assets/Scripts/Data/RagaTrackInfo.cs
+++ Assets/Scripts/Data/RagaTrackInfo.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -11,26 +12,55 @@ namespace ClinicalGames.Ragasutra
         Sarod, 
         Veena,
         Violin,
-        Santoor
+        Santoor,
+        Tabla,
+        Harmonium
     }
 
     [System.Serializable]
     public class RagaTrackInfo
     {
-        [BoxGroup("Audio Source", centerLabel: true)]
+        [BoxGroup("General", centerLabel: true)]
         [SerializeField] private AudioClip m_Clip;
-
-        [BoxGroup("Artist Details", centerLabel: true)]
+        [BoxGroup("General")]
         [SerializeField] private string m_ArtistName;
-        [SerializeField] private InstrumentType m_InstrumentType;
-
-        [BoxGroup("Playback Settings", centerLabel: true)]
+        [BoxGroup("General")]
         [Range(0F, 1F)]
         [SerializeField] private float m_VolumeAdjustment;
+        [BoxGroup("General")]
+        [SerializeField] private List<InstrumentType> m_Instruments;
 
         public AudioClip Clip => m_Clip;
         public string ArtistName => m_ArtistName;
-        public InstrumentType Instrument => m_InstrumentType;
+        public List<InstrumentType> Instruments => m_Instruments;
         public float VolumeAdjustment => m_VolumeAdjustment;
+        
+#if UNITY_EDITOR
+
+        /// <summary>
+        /// Removes Duplicate Tags from the List
+        /// Called By RagaData From OnValidate
+        /// </summary>
+        public void RemoveDuplicateTags()
+        {
+            if (m_Instruments != null && m_Instruments.Count > 0)
+            {
+                Dictionary<InstrumentType, bool> duplicates = new Dictionary<InstrumentType, bool>();
+                for (int i = 0; i < m_Instruments.Count; i++)
+                {
+                    if (duplicates.ContainsKey(m_Instruments[i])) continue;
+                    duplicates.Add(m_Instruments[i], true);
+                }
+
+                if (duplicates.Count == m_Instruments.Count) return;
+                m_Instruments.Clear();
+                foreach (InstrumentType instrumentType in duplicates.Keys)
+                {
+                    m_Instruments.Add(instrumentType);
+                }
+            }            
+        }
+        
+#endif
     }
 }
